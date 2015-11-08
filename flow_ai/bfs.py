0c9b0f5f1b04b19f69c2065561eba59f392d1d3a
Nick Whitehill <nicholas.whitehill@gmail.com>
2015-11-07 17:13:47 -0700

    Initial BFS implementation

--- flow_ai/bfs.py
+++ flow_ai/bfs.py
@@ -1,40 +1,83 @@
 from flow_game.flow_board import Flow, Path
 import flow_game.utilities as utils
-from collections import deque
+from copy import deepcopy
 
 
 class BFS:
     """
-    :type queue: deque([Flow])
+    :type queue: List[Flow]
     """
 
     def __init__(self):
-        self.queue = deque([])
+        self.queue = []
 
     def solve(self, filename):
         start = utils.load_game(filename)
+        return self.round_robin_solve(start)
 
-    def round_robin_solve(self, initial_game_state, colors):
+    def round_robin_solve(self, initial_game_state):
         """
         Creates the stack to be searched in a round robin manner - Each color gets to select 1 move until the solution
         is found.
         :type initial_game_state: Flow
-        :type colors: list[Path]
         :return: Flow
         """
         self.queue.append(initial_game_state)
 
+        # For loop to go through queue
         for game in self.queue:
-            grow_points1 = game.
+            if utils.at_goal(game):
+                return game
+            self.generate_possible_moves_rr(game)
+            self.queue.remove(game)
+        return None
 
+    def generate_possible_moves_rr(self, game):
+        """
+        Takes in a game and updates the queue. Games are added as if each grow point got a turn to pick a space.
+        Colors will alternate.
 
+        :type game: Flow
+        """
+        gp1_games = []
+        gp2_games = []
+        for color in game.paths:
+            # TODO clean up expression
+            path = game.paths[color]
+            if path.is_complete():
+                continue
 
+            # Get grow points and points adjacent to them
+            gp1, gp2 = path.get_grow_points()
+            adj2gp1 = utils.get_adjacent_points(gp1)
+            adj2gp2 = utils.get_adjacent_points(gp2)
 
+            # Handle adding adjacent points to grow points separately - in order to maintain RR order.
+            for possible in adj2gp1:
+                if path.can_be_added_to_path(possible, 1):
+                    copy_game = deepcopy(game)
+                    """:type: Flow"""
+                    copy_game.paths[color].add_to_path(possible, 1)
+                    gp1_games.append(copy_game)
 
+            for possible in adj2gp2:
+                if path.can_be_added_to_path(possible, 1):
+                    copy_game = deepcopy(game)
+                    """:type: Flow"""
+                    copy_game.paths[color].add_to_path(possible, 2)
+                    gp2_games.append(copy_game)
 
-
-
+        # Add games to queue
+        self.queue += gp1_games + gp2_games
 
 
 if __name__ == '__main__':
-    print "bfs"
+    simple_board = [['R', 'Y', '0'],
+                    ['0', '0', '0'],
+                    ['R', '0', 'Y']]
+
+    simple_flow = Flow(simple_board)
+
+    print BFS().round_robin_solve(simple_flow)
+
+    print BFS().solve("../TestBoards/easy5x5.txt")
